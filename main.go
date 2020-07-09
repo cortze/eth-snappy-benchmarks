3ce7cd7ebc7bb65276223a286ae47252c5355e0c
Mikel Cortes <Cortze>
2020-07-09 10:45:44 +0200

    Added iteration throught the same blocks to get a better average, and added real eth blocks for a more appropiate benchmark

--- main.go
+++ main.go
@@ -37,72 +37,73 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	for i := 0; i < 50; i++ {
+		// Iterate through the json files to make the compression test
+		for _, file := range files {
 
-	// Iterate through the json files to make the compression test
-	for _, file := range files {
+			fmt.Println(file)
 
-		fmt.Println(file)
+			// filter the files that finishes in .json
+			if strings.HasSuffix(file, *suffix) {
 
-		// filter the files that finishes in .json
-		if strings.HasSuffix(file, *suffix) {
+				// Open our jsonFile
+				jsonFile, err := os.Open(file)
 
-			// Open our jsonFile
-			jsonFile, err := os.Open(file)
-
-			// if we os.Open returns an error then handle it
-			if err != nil {
-				fmt.Println(err)
-			} else {
-
-				// defer the closing of our jsonFile so that we can parse it later on
-				// Read the opened jsonFile as a byte array
-				byteValue, _ := ioutil.ReadAll(jsonFile)
-				fmt.Println("Size block (bytes):            ", len(byteValue))
-
-				// Start the timer
-				start1 := time.Now()
-
-				// Compress the message with the snappy compressor
-				compressmsg := snappy.Encode(nil, byteValue)
+				// if we os.Open returns an error then handle it
 				if err != nil {
-					fmt.Println("Encode Failed")
+					fmt.Println(err)
+				} else {
+
+					// defer the closing of our jsonFile so that we can parse it later on
+					// Read the opened jsonFile as a byte array
+					byteValue, _ := ioutil.ReadAll(jsonFile)
+					fmt.Println("Size block (bytes):            ", len(byteValue))
+
+					// Start the timer
+					start1 := time.Now()
+
+					// Compress the message with the snappy compressor
+					compressmsg := snappy.Encode(nil, byteValue)
+					if err != nil {
+						fmt.Println("Encode Failed")
+					}
+
+					fmt.Println("Size block compressed (bytes): ", len(compressmsg))
+
+					// Stop the timer
+					codetime := time.Since(start1)
+					ctime := float64(codetime*time.Microsecond) / float64(time.Millisecond)
+
+					fmt.Println("Encoding time:     ", codetime)
+
+					// Run the timer
+					start2 := time.Now()
+
+					//fmt.Println("Message Compressed: ", compressmsg.Data)
+
+					// Decode the message
+					_, err = snappy.Decode(nil, compressmsg)
+					if err != nil {
+						fmt.Println("Decode Failed")
+					}
+
+					decodetime := time.Since(start2)
+
+					compressRatio := (float64(len(byteValue)) / float64(len(compressmsg)))
+					compressSpeed := (float64(len(byteValue)) / ctime)
+
+					fmt.Println("Decoding time:     ", decodetime)
+					fmt.Println("Compression ratio: ", compressRatio)
+					fmt.Println("Compression Speed: ", compressSpeed, "MB/s")
+
+					avgCompresRatio = append(avgCompresRatio, compressRatio)
+					avgCompresSpeed = append(avgCompresSpeed, compressSpeed)
+
+					fmt.Printf("\n")
 				}
 
-				fmt.Println("Size block compressed (bytes): ", len(compressmsg))
-
-				// Stop the timer
-				codetime := time.Since(start1)
-				ctime := float64(codetime*time.Microsecond) / float64(time.Millisecond)
-
-				fmt.Println("Encoding time:     ", codetime)
-
-				// Run the timer
-				start2 := time.Now()
-
-				//fmt.Println("Message Compressed: ", compressmsg.Data)
-
-				// Decode the message
-				_, err = snappy.Decode(nil, compressmsg)
-				if err != nil {
-					fmt.Println("Decode Failed")
-				}
-
-				decodetime := time.Since(start2)
-
-				compressRatio := (float64(len(byteValue)) / float64(len(compressmsg)))
-				compressSpeed := (float64(len(byteValue)) / ctime)
-
-				fmt.Println("Decoding time:     ", decodetime)
-				fmt.Println("Compression ratio: ", compressRatio)
-				fmt.Println("Compression Speed: ", compressSpeed, "MB/s")
-
-				avgCompresRatio = append(avgCompresRatio, compressRatio)
-				avgCompresSpeed = append(avgCompresSpeed, compressSpeed)
-
-				fmt.Printf("\n")
+				defer jsonFile.Close()
 			}
-
-			defer jsonFile.Close()
 		}
 	}
 	var avgRatio float64 = 0
@@ -113,7 +114,25 @@ func main() {
 		avgRatio = avgRatio + avgCompresRatio[i]
 		avgSpeed = avgSpeed + avgCompresSpeed[i]
 	}
+	ratioMin, ratioMax := findMinAndMax(avgCompresRatio)
+	speedMin, speedMax := findMinAndMax(avgCompresSpeed)
 	fmt.Println("Average Compress Ratio: ", avgRatio/float64(len(avgCompresRatio)))
+	fmt.Println("Max Ratio: ", ratioMax, "Min Ratio: ", ratioMin)
 	fmt.Println("Average Compress Speed: ", avgSpeed/float64(len(avgCompresSpeed)), "MB/s")
+	fmt.Println("Max Speed: ", speedMax, "Min Speed: ", speedMin)
 
+}
+
+func findMinAndMax(a []float64) (min float64, max float64) {
+	min = a[0]
+	max = a[0]
+	for _, value := range a {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
 }
